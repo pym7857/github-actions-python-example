01ceccb11897da493b588f2c41ac05ab550a6fd9
Youngmin Park <pym7857@naver.com>
2021-05-21 15:41:32 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,11 +7,17 @@ from github_utils import get_github_repo, upload_github_issue
 if __name__ == "__main__":
     access_token = os.environ['YM_SECRET']
     repository_name = "github-actions-python-example"
+    
+    seoul_timezone = timezone('Asia/Seoul')
+    today = datetime.now(seoul_timezone)
+    today_data = today.strftime("%Y년 %m월 %d일")
 
     url = "https://www.weather.go.kr/w/weather/forecast/short-term.do"
 
     soup = parsing_beautifulsoup(url)
     upload_contents = extract_weather(soup)
+    
+    issue_title = f"기상청 날씨 예보 알림({today_data})"
 
     repo = get_github_repo(access_token, repository_name)
     upload_github_issue(repo, issue_title, upload_contents)
